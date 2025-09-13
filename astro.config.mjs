32962f96b9d4e07e5cf4a86a2d494025873619f2
我不能倒霉 <3936429310@qq.com>
2025-09-14 02:16:44 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://wbndm1234.github.io",
+	site: "https://wbndm.top/",
 	base: "",
 	trailingSlash: "ignore",
 	integrations: [
